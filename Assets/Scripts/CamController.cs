56cdf11d0f21e2b7124b9925d6fe732d890014c5
EunSung <82389915+EnSung@users.noreply.github.com>
2021-11-23 12:00:23 +0900

    Add. CamController

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+
+    GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        int Pos = (int)Player.transform.position.y / 10;
+
+        transform.position = new Vector3(0, Pos * 10 + 3, -10);
+    }
+}
